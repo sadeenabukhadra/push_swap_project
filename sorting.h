9ca81fc11fe422a60ee2b8bc6b20f780eb2c9034
Sadeen Abu khadra <sabu-kha@c1r7s5.irbid.42.school>
2026-03-12 17:31:59 +0300

    complete benech

--- sorting.h
+++ sorting.h
@@ -1,40 +1,23 @@
-#ifndef PUSH_SWAP
-#define PUSH_SWAP
-#include "sorting.h"
+#ifndef SORTING_H
+#define SORTING_H
 #include <stddef.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-
-
-typedef struct s_node
+typedef struct s_ops
 {
-    int             value;
-    int             index;
-    struct s_node   *next;
-}   t_node;
+    int sa;
+    int sb;
+    int ss;
+    int pa;
+    int pb;
+    int ra;
+    int rb;
+    int rra;
+    int rrb;
+    int rrr;
+}   t_ops;
 
-typedef struct s_stack
-{
-    t_node  *top;
-    int     size;
-}   t_stack;
-
-void swap(t_stack *stack);
-void sa(t_stack *a);
-void sb(t_stack *b);
-void ss(t_stack *a, t_stack *b);
-void push(t_stack *src, t_stack *dest);
-void pa(t_stack *a, t_stack *b);
-void pb(t_stack *a, t_stack *b);
-void rotate(t_stack *stack);
-void ra(t_stack *a);
-void rb(t_stack *b);
-void rr(t_stack *a, t_stack *b);
-void reverse_rotate(t_stack *stack);
-void rra(t_stack *a);
-void rrb(t_stack *b);
-void rrr(t_stack *a, t_stack *b);
 int is_sorted(t_stack *a);
 void sort_three(t_stack *a);
 void sort_five(t_stack *a, t_stack *b);
@@ -57,6 +40,3 @@ int is_valid_number(char *str);
 int	ft_isdigit(int c);
 void parse_input(int ac, char *argv, t_stack *a);
 char **split_args(char *arg);
-
-#endif
-
--- src/utils/bench.c
+++ src/utils/bench.c
@@ -1,9 +1,71 @@
 #include "../../push_swap.h"
-void print_stack(t_stack *a)
+double compute_disorder(int *arr, int size)
 {
-    t_node *tmp;
-    while(tmp)
+    int i;
+    int j;
+    int mistakes;
+    int total_pair;
+    i=0;
+    mistakes=0;
+    total_pair=0;
+    while(i<size)
     {
-        
+        j=i+1;
+        while (j<size)
+        {
+            total_pair++;
+            if(arr[i]>arr[j])
+                mistakes++;
+            j++;
+        }
+        i++;      
     }
+    if(total_pair==0)
+        return(0) ;
+    return((double)mistakes/total_pair);                                                                                                                                                                            
+}
+int get_total_ops(t_ops *ops)
+{
+    return(
+        ops->sa+ops->sb+ops->ss+
+        ops->pa+ops->pb+
+        ops->ra+ops->rb+ops->rr+                                                                         
+        ops->rra+ops->rrb+ops->rrr);
+}
+static void print_line(char *name,int value)
+{
+    write(2,name,ft_strlen(name));
+    ft_putnbr_fd(value,2);
+    write(2,"\n",1);
+}
+static void print_operations(t_ops *ops)
+{
+    print_line("sa:",ops->sa);
+    print_line("sb",ops->sb);
+    print_line("ss:",ops->ss);
+    print_line("pb:",ops->pb);
+    print_line("pa:",ops->pa);
+    print_line("ra:",ops->ra);
+    print_line("rb:",ops->rb);
+    print_line("rr:",ops->rr);
+    print_line("rra:",ops->rra);
+    print_line("rrb:", ops->rrb);
+    print_line("rrr:",ops->rrr);
+}
+
+void	print_bench(double disorder, char *strategy, t_ops *ops)
+{
+	int	total;
+
+	total = get_total_ops(ops);
+	write(2, "Disorder: ", 10);
+	ft_putnbr_fd((int)(disorder * 100), 2);
+	write(2, "\n", 2);
+	write(2, "Strategy: ", 10);
+	write(2, strategy, ft_strlen(strategy));
+	write(2, "\n", 1);
+	write(2, "Total operations: ", 18);
+	ft_putnbr_fd(total, 2);
+	write(2, "\n\n", 2);
+	print_operations(ops);
 }
